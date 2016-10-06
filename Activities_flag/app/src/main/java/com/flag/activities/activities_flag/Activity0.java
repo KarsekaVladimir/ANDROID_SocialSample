64abe8ca5ea63a4fda05333323246897e886c8dc
KarsekaVladimir <karsekavl@gmail.com>
2016-10-06 14:36:13 +0300

    set Activityes Project

---
+++ Activities_flag/app/src/main/java/com/flag/activities/activities_flag/Activity0.java
@@ -0,0 +1,13 @@
+package com.flag.activities.activities_flag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_0);
+    }
+}
