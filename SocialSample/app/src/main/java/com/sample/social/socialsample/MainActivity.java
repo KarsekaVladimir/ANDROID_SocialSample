0d1fdb53eff18ee8fd5cdb2306706e4520fad7df
KarsekaVladimir <karsekavl@gmail.com>
2016-10-07 13:49:13 +0300

    create custom attributes for different buttons

--- SocialSample/app/src/main/java/com/sample/social/socialsample/MainActivity.java
+++ SocialSample/app/src/main/java/com/sample/social/socialsample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sample.social.socialsample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //TextView textView = (TextView) findViewById(R.id.activity_main);
-    //textView.setText("")
+    public void toMain2Activity (View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);}
 }
